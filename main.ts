4ab0c8b3dc33cae584ea75ac4b00aa42e606de5f
luisafuentesb <112406796+luisafuentesb@users.noreply.github.com>
2022-09-10 18:38:12 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.showIcon(IconNames.Asleep)
+    basic.pause(1000)
+})
